7c6ca8159a7a07c7f3725379ec31b5527df9546e
disne_y <bold.disney@gmail.com>
2018-04-13 13:43:55 +0300

    Add rate update action

--- app/controllers/exchange_rates_controller.rb
+++ app/controllers/exchange_rates_controller.rb
@@ -1,13 +1,21 @@
 class ExchangeRatesController < ApplicationController
-  after_action :update_exchange_rate_channel, only: :create
+  before_action :set_rate, only: :update
+  after_action  :update_exchange_rate_channel, only: %i[create update]
 
   def new
-    @rate = ExchangeRate.new
+    @rate = ExchangeRate.last || ExchangeRate.new
   end
 
   def create
     @rate = ExchangeRate.new(exchange_rate_params)
     @rate.save
+  end
+
+  def update
+    @rate.update(exchange_rate_params)
+    respond_to do |format|
+      format.js { render 'create.js.erb' }
+    end
   end
 
   private
@@ -19,5 +27,9 @@ class ExchangeRatesController < ApplicationController
   #можно использовать callback в модели, но это вопрос для обсуждения
   def update_exchange_rate_channel
     ExchangeRatesJob.perform_later
+  end
+
+  def set_rate
+    @rate = ExchangeRate.find(params[:id])
   end
 end
