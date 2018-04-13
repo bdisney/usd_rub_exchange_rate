1358b4359c05c8f28ed58407943f292b069ad013
disne_y <bold.disney@gmail.com>
2018-04-13 11:28:31 +0300

    Add new and create action. Perform ExchangeRateJob after rate was created

--- app/controllers/exchange_rates_controller.rb
+++ app/controllers/exchange_rates_controller.rb
@@ -1,5 +1,23 @@
 class ExchangeRatesController < ApplicationController
+  after_action :update_exchange_rate_channel, only: :create
+
   def new
     @rate = ExchangeRate.new
+  end
+
+  def create
+    @rate = ExchangeRate.new(exchange_rate_params)
+    @rate.save
+  end
+
+  private
+
+  def exchange_rate_params
+    params.require(:exchange_rate).permit(:id, :value, :valid_until)
+  end
+
+  #можно использовать callback в модели, но это вопрос для обсуждения
+  def update_exchange_rate_channel
+    ExchangeRatesJob.perform_now
   end
 end
