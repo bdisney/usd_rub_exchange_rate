45fbb9fdc1bf1cbc8b82fe8e91568a7cb6688304
disne_y <bold.disney@gmail.com>
2018-04-14 19:59:21 +0300

    Minor fix

--- spec/jobs/exchange_rates_job_spec.rb
+++ spec/jobs/exchange_rates_job_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe ExchangeRatesJob, type: :job do
   describe "#perform_later" do
-    it "uploads a backup" do
+    it 'sends exchange rates updates to the channel' do
       ActiveJob::Base.queue_adapter = :test
       expect { ExchangeRatesJob.perform_later }.to have_enqueued_job
+      expect(ExchangeRatesJob).to have_been_enqueued.exactly(:once)
     end
   end
 end
