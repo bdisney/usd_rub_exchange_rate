5411a97dec43cfc79bca0e7575029fab02a6b53e
disne_y <bold.disney@gmail.com>
2018-04-15 20:20:36 +0300

    Minor fixes

--- spec/services/connection_spec.rb
+++ spec/services/connection_spec.rb
@@ -5,7 +5,6 @@ describe Connection do
     it 'calls Net::HTTP.get_response which returns a response' do
       VCR.use_cassette 'connection' do
         response = Connection.get(ExchangeRate::SITE_URL, ExchangeRate::APP_ID, ExchangeRate::RUB_SYMBOL)
-        expect(response.code).to eq ('200')
         expect(response.body).is_a?(String)
       end
     end
